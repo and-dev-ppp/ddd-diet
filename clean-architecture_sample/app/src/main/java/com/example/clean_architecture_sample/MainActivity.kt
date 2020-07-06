1baaf7490e794b29c3410a097dc5b585b6d07e0c
sei13 <kamkamsirona13@gmail.com>
2020-07-06 21:45:15 +0900

    下準備

--- clean-architecture_sample/app/src/main/java/com/example/clean_architecture_sample/MainActivity.kt
+++ clean-architecture_sample/app/src/main/java/com/example/clean_architecture_sample/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.clean_architecture_sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.clean_architecture_sample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
+    private val viewModel by viewModels<TopViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = this
     }
 }
---
+++ clean-architecture_sample/app/src/main/java/com/example/clean_architecture_sample/TopViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.clean_architecture_sample
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class TopViewModel: ViewModel(){
+
+    val fat = MutableLiveData("けんはデブ")
+}
